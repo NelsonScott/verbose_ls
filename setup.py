e7cc08bd8cb26397db763c23a1832202e611e799
Scott Nelson <scott.nelson@onepeloton>
2019-12-26 20:51:18 -0500

    update to set proj desc in pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+from os import path
 from setuptools import find_packages, setup
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name="verbose_ls",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version=VERSION,
     description="Format 'ls' output into verbose, human readable output",
     url="https://github.com/NelsonScott/verbose_ls",
